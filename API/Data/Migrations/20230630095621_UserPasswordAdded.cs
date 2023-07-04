96f727cfdb8004fc1ec6fe8c4738e4bcf2bfccce
b-sosic <brankasosic@digitalcontrol.me>
2023-07-04 09:12:39 +0200

    End of section4

---
+++ API/Data/Migrations/20230630095621_UserPasswordAdded.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserPasswordAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordHash",
+                table: "MyProperty",
+                type: "BLOB",
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordSalt",
+                table: "MyProperty",
+                type: "BLOB",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordHash",
+                table: "MyProperty");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordSalt",
+                table: "MyProperty");
+        }
+    }
+}
