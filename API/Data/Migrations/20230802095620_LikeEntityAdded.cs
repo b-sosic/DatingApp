38dc12dfc1538584ccfc0a0d64dee7b8eab8d2c5
b-sosic <brankasosic@digitalcontrol.me>
2023-08-02 15:09:59 +0200

    End of section 14

---
+++ API/Data/Migrations/20230802095620_LikeEntityAdded.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class LikeEntityAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ID",
+                table: "Photos",
+                newName: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "Likes",
+                columns: table => new
+                {
+                    SourceUserId = table.Column<int>(type: "INTEGER", nullable: false),
+                    TargetUserId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Likes", x => new { x.SourceUserId, x.TargetUserId });
+                    table.ForeignKey(
+                        name: "FK_Likes_MyProperty_SourceUserId",
+                        column: x => x.SourceUserId,
+                        principalTable: "MyProperty",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Likes_MyProperty_TargetUserId",
+                        column: x => x.TargetUserId,
+                        principalTable: "MyProperty",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Likes_TargetUserId",
+                table: "Likes",
+                column: "TargetUserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Likes");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Photos",
+                newName: "ID");
+        }
+    }
+}
--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -29,11 +29,8 @@ namespace API.Entities
         public string Country { get; set; }
         public List<Photo> Photos { get; set; } = new ();
     
-    
-        // public int GetAge()
-        // {
-        //     return DateofBirth.CalculateAge();
-        // }
+        public List<UserLike> LikedByUsers { get; set; }
+        public List<UserLike> LikedUsers { get; set; }
     }
 
 
